cdb1773601f7e98783ea6741aea469a69f927773
Kriti Shankar <kritiara1010@gmail.com>
2026-02-17 15:20:52 +0530

    GenZ Classes

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector("button").addEventListener("click", () => {
+    alert("Welcome to Gen Z Class 🚀");
+});
+
+document.getElementById("admissionForm")
+.addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Form submitted successfully 🚀 Gen Z Class will contact you soon!");
+});
